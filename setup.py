b88bc2a2443b6c4a1c8d060f7898c745814b888e
= <=>
2015-01-25 22:26:14 -0700

    Don't install enum34 on python 3.4

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 
 from distutils.core import setup
+import sys
+
+install_requires = [
+    "future>=0.14.3",
+    "cryptography>=0.7.2"
+]
+if sys.version_info < (3,4):
+    install_requires.extend([
+        "enum34>=1.0.4"
+    ])
 
 setup(name='pyssh',
       version=0.1,
@@ -7,10 +17,5 @@ setup(name='pyssh',
       author='Jacob Beck',
       author_email='beckjake@gmail.com',
       packages=['pyssh'],
-      install_requires=[
-        "future>=0.14.3",
-        "cryptography>=0.7.2",
-        "singledispatch>=3.4.0.3",
-        "enum34>=1.0.4"
-        ]
+      install_requires=install_requires
 )
